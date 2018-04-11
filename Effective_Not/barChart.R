30f57b61f38e72064649fefc13122d16d78b4836
Ashwin Lakshmanan <ashwin2@ncsu.edu>
2018-04-11 14:30:47 -0400

    Reverting indentation

--- Effective_Not/barChart.R
+++ Effective_Not/barChart.R
@@ -9,10 +9,4 @@ ugmajor$Breakdown <- as.numeric(ugmajor$Breakdown)
 ugmajor$Major <- factor(ugmajor$Major, levels=ugmajor[order(ugmajor$Breakdown, decreasing = TRUE), "Major"])
 
 # Plot it!
-ggplot(ugmajor, aes(x = Major, y = Breakdown)) 
-  + geom_bar(stat="identity", fill = "#EC842D", colour = "black") 
-  + scale_y_continuous(breaks = c(5, 10, 15, 20, 25, 30, 35)) 
-  + scale_x_discrete(labels = gsub(" ", "\n", c("Business", "Economics", "Engine- ering", "Liberal Arts/ Sciences", "Physical Sciences", "Other", "Law"))) 
-  + theme_solarized() 
-  + theme(legend.position = "none") 
-  + ggtitle("% Breakdown of Undergraduate Major")
+ggplot(ugmajor, aes(x = Major, y = Breakdown)) + geom_bar(stat="identity", fill = "#EC842D", colour = "black") + scale_y_continuous(breaks = c(5, 10, 15, 20, 25, 30, 35)) + scale_x_discrete(labels = gsub(" ", "\n", c("Business", "Economics", "Engine- ering", "Liberal Arts/ Sciences", "Physical Sciences", "Other", "Law"))) + theme_solarized() + theme(legend.position = "none") + ggtitle("% Breakdown of Undergraduate Major")
